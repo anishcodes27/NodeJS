a368e952a3e2bac69432c0c6eb75daa259576ac8
Anish Kumar <anish.kumar232@lpu.in>
2026-02-19 13:56:49 +0530

    updated

--- index4a.js
+++ index4a.js
@@ -3,10 +3,26 @@ const app = express();
 app.listen(8080, () => {
   console.log("Server Started");
 });
-app.get("/:num1/:num2", (req, resp) => {
-  console.log(req.url);
-  console.log(req.params);
-  const result = Number(req.params.num1) + Number(req.params.num2);
-    resp.send(result);
 
+
+
+
+// app.get("/:num1/:num2/:num3", (req, resp) => {
+//     console.log(req.url);
+//     console.log(req.params);
+//     const result = Number(req.params.num1) + Number(req.params.num2) +  Number(req.params.num3);
+//     resp.send(result);
+//     }
+// );
+
+
+// app.get("/x/:num1/y/:num2/z/:num3", (req, resp) => {
+//   console.log(req.url);
+//   console.log(req.params);
+//   const result = Number(req.params.num1) + Number(req.params.num2) +  Number(req.params.num3);
+//   resp.send(result);
+// });
+
+app.get("/a/b/c/d", (req, resp) => {
+  resp.send("hello student");
 });
