26bb7ef695f1fa477fcae52b90dd4bb701aaf66c
Anish Kumar <anish.kumar232@lpu.in>
2026-02-19 11:34:34 +0530

    updated

--- index3.js
+++ index3.js
@@ -3,6 +3,10 @@ const app=express()
 app.listen(8080,()=>{
     console.log("Server Started");
 });
+app.get("/",(req,resp)=>{
+    console.log(req.url)
+    resp.send("response from server for /")
+})
 app.get("/home",(req,resp)=>{
     console.log(req.url)
     resp.send("response from server for /home")
