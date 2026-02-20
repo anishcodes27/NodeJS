9375bed2ee076edfcb80a37780622b8d5665173f
Anish Kumar <anish.kumar232@lpu.in>
2026-02-20 15:50:21 +0530

    updated

--- index9a.js
+++ index9a.js
@@ -2,9 +2,23 @@ import express from "express";
 const app = express();
 app.listen(8080);
 
+//now we will send 1234 in the json format from the postman if it mathces then next otherwise it will be denied
+
+
+// const auth=(req,resp,next)=>{
+//    if(req.path==="/1234"){
+//     next();
+//    }
+//    else{
+//     resp.send("Access Denied");
+//    }
+// }
+
+
+app.use(express.json());
 
 const auth=(req,resp,next)=>{
-   if(req.path==="/1/2/3/4"){
+   if(req.body.token === 1234){
     next();
    }
    else{
@@ -13,12 +27,16 @@ const auth=(req,resp,next)=>{
 }
 
 
+
+
 app.use(auth);
 // app.use(logger);
-app.get("/1/2/3/4", (req, res) => {
+app.post("/1234", (req, res) => {
 
   res.send("Welcome");
 });
+
+
 
 
 
